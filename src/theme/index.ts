30e88dd5bd53b45154364237732c2d5698ce43d2
excavatefinance <81165639+excavatefinance@users.noreply.github.com>
2021-04-09 21:45:17 +0100

    Update index.ts

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -27,7 +27,7 @@ export interface PancakeTheme {
 }
 
 export { default as dark } from "./dark";
-export { default as light } from "./light";
+// export { default as light } from "./light";
 
 export { lightColors } from "./colors";
 export { darkColors } from "./colors";
